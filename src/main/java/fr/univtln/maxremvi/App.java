1492ee010864404b6dccad477fa54772b8193cac
remimomprive <rm.mc@hotmail.fr>
2017-10-12 21:31:28 +0200

    Premier commit

---
+++ src/main/java/fr/univtln/maxremvi/App.java
@@ -0,0 +1,13 @@
+package fr.univtln.maxremvi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
