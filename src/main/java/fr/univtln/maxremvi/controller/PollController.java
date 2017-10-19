7b16407d8dd741d70b195c401c05bbcb27336710
remimomprive <rm.mc@hotmail.fr>
2017-10-19 21:29:07 +0200

    Ajout de la suppression de réponse dans la vue CreatePoll

--- src/main/java/fr/univtln/maxremvi/controller/PollController.java
+++ src/main/java/fr/univtln/maxremvi/controller/PollController.java
@@ -2,12 +2,10 @@ package fr.univtln.maxremvi.controller;
 
 import fr.univtln.maxremvi.model.Person;
 import fr.univtln.maxremvi.model.Poll;
-import fr.univtln.maxremvi.database.AbstractDao;
 import fr.univtln.maxremvi.database.DatabaseDao;
 import fr.univtln.maxremvi.database.PollDao;
 
 import java.sql.*;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -29,7 +27,7 @@ public class PollController {
         return pollController;
     }
 
-    public Poll addPoll(String title, String description, String location, Date closingDate, boolean closed, Person promoter, boolean multipleChoice, boolean hideAnswers, boolean addDates, Poll.type pollType) throws SQLException {
+    public Poll addPoll(String title, String description, String location, Date closingDate, boolean closed, Person promoter, boolean multipleChoice, boolean hideAnswers, boolean addDates, Poll.pollType pollType) throws SQLException {
         return pollDao.add(new Poll(null, title, description, location, closingDate, closed, promoter, multipleChoice, hideAnswers, addDates, pollType));
     }
 
